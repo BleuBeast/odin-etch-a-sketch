43f6525f8d4f0c6025127ecfe56c3c427ae2f91b
Tim Baldwin <timwb499@gmail.com>
2024-05-08 00:21:25 -0400

    Updated Etch A Sketch to include dynamic sizing It will now resize individual pixels

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function drawSquare(value)
 {
     const container = document.querySelector(".container");
+    container.innerHTML = '';
     const square = document.createElement("div");
-    square.classList.add("square");
+    let size = 500/value;
+    square.style.height = size + "px";
+    square.style.width = size + "px";
 
     for(let i = 0; i < (value * value); i++)
     {
@@ -12,6 +15,18 @@ function drawSquare(value)
         });
         container.appendChild(tmp_square);
     }
+}
+
+function pickSize()
+{
+    var selection = "";
+    do
+    {
+        selection = parseInt(window.prompt("Please enter a number from 1 to 100"), "");
+    }
+    while(isNaN(selection) || selection > 100 || selection < 1);
+
+    drawSquare(selection);
 }
 
 drawSquare(16)
